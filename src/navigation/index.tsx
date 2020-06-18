e3d86fbcfdd3b1d8fcaa6f10b6fddb19e3741f6e
slamser56 <slamser56@gmail.com>
2020-06-18 16:51:08 +0300

    style(navigation)

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -3,9 +3,9 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {MainRoutes} from './StackNavigationRoutes';
 import profileStack from './profile';
-import RegistrationStack from './registration';
+import registrationStack from './registration';
 import main from '../pages/main';
-import SignInStack from './signIn';
+import signInStack from './signIn';
 
 const RootStack = createStackNavigator();
 
@@ -14,8 +14,8 @@ export default function Navigation(): ReactElement {
     <NavigationContainer>
       <RootStack.Navigator>
         <RootStack.Screen name={MainRoutes.MAIN} component={main} options={{ headerShown: false }} />
-        <RootStack.Screen name={MainRoutes.REGISTRATION_STACK} component={RegistrationStack} options={{ headerShown: false }} />
-        <RootStack.Screen name={MainRoutes.SIGN_IN_STACK} component={SignInStack} options={{ headerShown: false }} />
+        <RootStack.Screen name={MainRoutes.REGISTRATION_STACK} component={registrationStack} options={{ headerShown: false }} />
+        <RootStack.Screen name={MainRoutes.SIGN_IN_STACK} component={signInStack} options={{ headerShown: false }} />
         <RootStack.Screen name={MainRoutes.PROFILE_STACK} component={profileStack} options={{ headerShown: false }} />
       </RootStack.Navigator>
     </NavigationContainer>
